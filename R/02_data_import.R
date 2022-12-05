baeaa6b7168d3cf232db4494e85065b9461345ac
Gabriel Paquette <gabriel.paquette@gmail.com>
2022-12-05 10:09:44 -0500

    2022 bixi data import

--- R/02_data_import.R
+++ R/02_data_import.R
@@ -2,6 +2,37 @@
 source("R/01_startup.R")
 
 
+# Function to read multiple csvs ------------------------------------------
+
+read_plus <- function(flnm) {
+  read_csv(flnm, show_col_types = F) %>% 
+    mutate(filename = flnm)
+}
+
 # Import 2022 BIXi data ---------------------------------------------------
+
+
+trips_22 <- 
+  list.files(path = "./data/",
+             pattern = "*deplacements.csv",
+             full.names = T) %>% 
+  map_df(~read_plus(.))
+
+stations_22 <- 
+  list.files(path = "./data/",
+             pattern = "*stations.csv",
+             full.names = T) %>% 
+  map_df(~read_plus(.))
+
+
+# testing -----------------------------------------------------------------
+
+stations_22 %>% group_by(pk) %>% count(latitude) %>% count(pk) %>% view()
+
+
+# Save processed data -----------------------------------------------------
+
+qsavem(trips_22, stations_22, file = "data/bixi_22.qs")
+rm(trips_22,stations_22)
 
 
