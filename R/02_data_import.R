d784af3bddd34b61710626601260425389ba140e
Gabriel Paquette <gabriel.paquette@gmail.com>
2022-12-10 19:46:02 -0500

    data imports

--- R/02_data_import.R
+++ R/02_data_import.R
@@ -2,37 +2,54 @@
 source("R/01_startup.R")
 
 
-# Function to read multiple csvs ------------------------------------------
+# Functions to read multiple csvs -----------------------------------------
 
-read_plus <- function(flnm) {
+read_bixi <- function(flnm) {
   read_csv(flnm, show_col_types = F) %>% 
     mutate(filename = flnm)
 }
 
-# Import 2022 BIXi data ---------------------------------------------------
+read_weather <- function(flnm) {
+  read_csv(flnm, show_col_types = F) %>% 
+    mutate(filename = flnm) %>% 
+    select(1:3,5,10,16,20,30)
+}
 
+# Import 2022 weather, BIXi, and metro exit data --------------------------
+
+weather_22 <- 
+  list.files(path = "./data/",
+           pattern = "*P1H.csv",
+           full.names = T) %>% 
+  map_df(~read_weather(.))
 
 trips_22 <- 
   list.files(path = "./data/",
              pattern = "*deplacements.csv",
              full.names = T) %>% 
-  map_df(~read_plus(.))
+  map_df(~read_bixi(.))
 
 stations_22 <- 
   list.files(path = "./data/",
              pattern = "*stations.csv",
              full.names = T) %>% 
-  map_df(~read_plus(.))
+  map_df(~read_bixi(.)) %>% 
+  select(-filename) %>% 
+  distinct() %>% 
+  filter(!name == "Cadillac / Sherbrooke") %>% 
+  filter(!name == "Place Émilie-Gamelin") %>% 
+  mutate(latitude = case_when((pk == 856) ~ 45.492899,
+                              TRUE ~ latitude)) %>% 
+  mutate(longitude = case_when((pk == 856) ~ -73.556447,
+                               TRUE ~ longitude))
 
-
-# testing -----------------------------------------------------------------
-
-stations_22 %>% group_by(pk) %>% count(latitude) %>% count(pk) %>% view()
-
+metro_exits <- 
+  read_csv("data/gtfs/stops.txt") %>% 
+  filter(location_type == 2)
 
 # Save processed data -----------------------------------------------------
 
 qsavem(trips_22, stations_22, file = "data/bixi_22.qs")
-rm(trips_22,stations_22)
-
-
+qsavem(weather_22, file = "data/weather_22.qs")
+qsavem(metro_exits, file = "data/metro.qs")
+rm(trips_22, stations_22, weather_22, metro_exits, read_bixi, read_weather)
